c3098d0c493aee12c22acdb23cd5b9baf24279ed
KarloVlahek <91391943+KarloVlahek@users.noreply.github.com>
2026-04-06 22:02:55 -0400

    updated resources to reflect naming convention of new data.

--- terraform/modules/bigquery/main.tf
+++ terraform/modules/bigquery/main.tf
@@ -1,4 +1,20 @@
-resource "google_bigquery_dataset" "oil_supply_demand_dataset" { #demo_dataset is the name of the resource within the TERRAFORM file we're working with
-  dataset_id = var.dataset_id
-  location   = var.location
+resource "google_bigquery_dataset" "oil_supply_demand_dataset" {
+  dataset_id  = var.dataset_id
+  location    = var.location
+  description = "EPA CEMS power plant emissions pipeline dataset"
+}
+
+resource "google_bigquery_table" "raw_cems_daily" {
+  dataset_id          = google_bigquery_dataset.oil_supply_demand_dataset.dataset_id
+  table_id            = "raw_cems_daily"
+  deletion_protection = false
+
+  time_partitioning {
+    type  = "DAY"
+    field = "date"
+  }
+
+  clustering = ["state", "facility_id"]
+
+  schema = file("${path.module}/schemas/raw_cems_hourly.json")
 }
